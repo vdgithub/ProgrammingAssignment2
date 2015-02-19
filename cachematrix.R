a3eb2fb22d11cb7a2275d33c23642eaa9a41d7ad
Vidhu Devaraj <vdgithub@gmail.com>
2015-02-18 23:07:17 -0600

    Coursera Programming Assignment 2- updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,40 @@
-##  These functions cache the inverse of a matrix instead of repeated computation.
-##  This saves a lot of computation time when huge matrices are involved.
+##  Matrix inverse can be a time consuming operation especially for huge matrices. 
+##  The functions below provide an example of how to cache the inverse of a matrix
+##  instead of repeated computation, thus improving the code performance.
+
 
 ##  The function below returns a list of 4 get and set functions
 ##  that create a special "matrix" object and cache its inverse.
 
 makeCacheMatrix <- function(x=matrix()){
   
-  ##Assign empty matrix
+  ##  Assign empty matrix
   Inv <- matrix()
  
-  ##Save the matrix
-  ## <<- operator makes the variables assessible outside setMatrix() function
+  ##  Save the matrix
+  ##  <<- operator makes the variables assessible outside setMatrix() function
   setMatrix <- function(y){
     
     x <<-y
     Inv <<- matrix()
   }
   
-  ##Retrive the matrix
+  ##  Retrive the matrix
   getMatrix <- function(){
     x
   }
   
-  ##Save Inverse of the matrix
+  ##  Save Inverse of the matrix
   setInverse <- function(inverse){
     Inv <<- inverse
   }
   
-  ##Retrive Inverse of the matrix
+  ##  Retrive Inverse of the matrix
   getInverse <- function(){
     Inv
   }
   
-  ##Returns the list of function names
+  ##  Returns the list of function names
   list(setMatrix=setMatrix,
        getMatrix=getMatrix,
        setInverse=setInverse,
@@ -41,22 +43,23 @@ makeCacheMatrix <- function(x=matrix()){
 
 
 ##  The function below computes inverse of the matrix returned by makeCacheMatrix function above. 
-##  If inverse already exists for the same matrix, then cached inverse is displayed with message "Caching Inverse".
+##  If inverse already exists for the matrix, then display the cached inverse.
+##  Else compute inverse and save the results.
 
 cacheSolve <- function(x, ...){
 
-  ##Get the saved inverse for matrix x
-  ##If inverse is not computed yet then empty matrix is returned
+  ##  Get the saved inverse for matrix x
+  ##  If inverse is not computed yet then empty matrix is returned
   Inv <- x$getInverse() 
   
-  ##Display cached Inverse if exists and return
+  ##  Display cached Inverse if exists and return
   if(sum(complete.cases(Inv))!=0){
     print("Caching Inverse")
     return(Inv)
   }
   
 
-  ##Else Compute and save Inverse
+  ##  Compute and save Inverse
   Inv <- solve(x$getMatrix(),...)  
   
   x$setInverse(Inv) 
